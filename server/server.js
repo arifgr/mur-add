7269011607a1696af29f8a913439f90afda3fb2d
arifgr <arifgur94@gmail.com>
2025-07-26 16:03:17 +0300

    feat(server): add user controller with register route

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
-// import userRouter from "./routes/userRoute.js";
+import userRouter from "./routes/userRoute.js";
 // import sellerRouter from "./routes/sellerRoute.js";
 // import connectCloudinary from "./configs/cloudinary.js";
 // import productRouter from "./routes/productRoute.js";
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
-// app.use("/api/user", userRouter);
+app.use("/api/user", userRouter);
 // app.use("/api/seller", sellerRouter);
 // app.use("/api/product", productRouter);
 // app.use("/api/cart", cartRouter);
