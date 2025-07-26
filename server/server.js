bf3ab97b547d09abc5e4494ad936d4d11c5091a6
arifgr <arifgur94@gmail.com>
2025-07-26 18:34:08 +0300

    feat(service): add seller controller authentication and middleware

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
-// import sellerRouter from "./routes/sellerRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
 // import connectCloudinary from "./configs/cloudinary.js";
 // import productRouter from "./routes/productRoute.js";
 // import cartRouter from "./routes/cartRoute.js";
@@ -30,7 +30,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
-// app.use("/api/seller", sellerRouter);
+app.use("/api/seller", sellerRouter);
 // app.use("/api/product", productRouter);
 // app.use("/api/cart", cartRouter);
 // app.use("/api/address", addressRouter);
