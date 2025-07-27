6e269ffd8ad3f957c5ec9d8bdc4a79a340122e6e
arifgr <arifgur94@gmail.com>
2025-07-27 12:56:26 +0300

    feat(server): add product route

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
-// import connectCloudinary from "./configs/cloudinary.js";
-// import productRouter from "./routes/productRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 // import cartRouter from "./routes/cartRoute.js";
 // import addressRouter from "./routes/addressRoute.js";
 // import orderRouter from "./routes/orderRoute.js";
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB();
-// await connectCloudinary();
+await connectCloudinary();
 
 // Allow multiple origins
 const allowedOrigins = ["http://localhost:5173", ""];
@@ -31,7 +31,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
-// app.use("/api/product", productRouter);
+app.use("/api/product", productRouter);
 // app.use("/api/cart", cartRouter);
 // app.use("/api/address", addressRouter);
 // app.use("/api/order", orderRouter);
